79b4c489e77941fe11219eb5b0727c8ee573a440
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-02-28 23:05:35 +0300

    add to global.d.ts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import './Header.css';
 
+
 const Header: FC = (): React.ReactElement => {
   return (
     <header className="header">
       <div className='header__container'>
         <div className='header__logo'>
-          <img src='' alt='logo' className='header__logo-img'/>
+          <img src='' alt="logo" className='header__logo-img'/>
         </div>
         <ul className='header__auth'>
           <li className='header__auth-item'><button type='button' className='header__button'>Регистрация</button></li>
