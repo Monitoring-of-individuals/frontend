af22d7173d738f4530dfaacce250fb2ae908edc1
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-03-01 01:08:43 +0300

    first version of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
 import React, { FC } from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer: FC = (): React.ReactElement => {
-  return <footer className="" />;
+  return (
+    <footer className='footer'>
+      <div className='footer__container'>
+        <ul className='footer__links'>
+          <li className='footer__link'>
+            <Link to="/#" className='footer__link-policy'>
+              Политика конфиденциальности
+            </Link>
+          </li>
+          <li className='footer__link'>
+            Связаться с нами <a className='footer__link-mail' href="mailto:monitoringofindividuals@gmail.com">monitoringofindividuals@gmail.com</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
