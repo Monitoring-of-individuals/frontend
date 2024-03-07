ff0b7070a77a32df5d5e854bdc5c563b1d60d862
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-03-07 22:53:07 +0300

    redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+// import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+// import rootReducer from './services/reducers/rootReducer';
+import store from './services/store/configureStore';
 // import { HashRouter } from 'react-router-dom'; // нужно заменить на него, при деплое через gh-pages
 import App from './components/App/App';
 
@@ -10,8 +14,10 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
