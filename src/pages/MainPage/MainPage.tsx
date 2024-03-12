bc94341e3ca8970cf2423cc32245e584e175240b
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-03-12 17:11:14 +0300

    redesign of main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react';
 import './MainPage.css';
 import Landing from '../../components/Landing/Landing';
 import About from '../../components/About/About';
+import Sources from '../../components/Sources/Sources';
 
 const MainPage: FC = (): React.ReactElement => {
   return (
     <main className='main'>
       <Landing />
       <About />
+      <Sources />
     </main>
   )
 }
