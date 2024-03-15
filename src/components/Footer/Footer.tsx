980e20d581eaf55fd979bc609f42362c3489f183
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-03-16 00:56:35 +0300

    redesign of footer and header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,23 +6,25 @@ const Footer: FC = (): React.ReactElement => {
   return (
     <footer className='footer'>
       <div className='footer__container'>
-        <ul className='footer__links'>
-          <li className='footer__link'>
-            <Link to="/#" className='footer__link-terms link'>
-              Условия пользования сервисом
-            </Link>
-          </li>
-          <li className='footer__link'>
-            <Link to="/#" className='footer__link-terms link'>
-              Политика конфиденциальности
-            </Link>
-          </li>
-        </ul>
-        <ul className='footer__links'>
-          <li className='footer__link'>
-            Связаться с нами <a className='footer__link-mail link' href="mailto:monitoringofindividuals@gmail.com">monitoringofindividuals@gmail.com</a>
-          </li>
-        </ul>
+        <nav className='footer__navigation'>
+          <ul className='footer__links'>
+            <li className='footer__link'>
+              <Link to="/#" className='footer__link-terms link'>
+                Условия пользования сервисом
+              </Link>
+            </li>
+            <li className='footer__link'>
+              <Link to="/#" className='footer__link-terms link'>
+                Политика конфиденциальности
+              </Link>
+            </li>
+          </ul>
+          <ul className='footer__links'>
+            <li className='footer__link'>
+              Связаться с нами <a className='footer__link-mail link' href="mailto:monitoringofindividuals@gmail.com">monitoringofindividuals@gmail.com</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg';
 import './Header.css';
 
 interface HeaderProps {
@@ -11,22 +12,30 @@ const Header: FC<HeaderProps> = ({ openSignUpPopup, openSignInPopup }): React.Re
   return (
     <header className="header">
       <div className="header__container">
-        {/* <div className="header__logo">
-          <img src="" alt="logo" className="header__logo-img" />
-        </div> */}
-        <p className='header__monitoring link'>Мониторинг физических лиц</p>
-        <ul className="header__auth">
-          <li className="header__auth-item link">
-            <Link to="/signup" className="header__button" onClick={openSignUpPopup}>
-              Регистрация
-            </Link>
-          </li>
-          <li className="header__auth-item link">
-            <Link to="/signin" className="header__button" onClick={openSignInPopup}>
-              Вход
-            </Link>
-          </li>
-        </ul>
+        <Link to='/' className='header__link-to-main'>
+          <div className="header__logo">
+            <img src={logo} alt="logo" className="header__logo-img" />
+          </div>
+          <p className='header__monitoring link'>Мониторинг физических лиц</p>
+        </Link>
+        <nav className='header__nav'>
+          <ul className='header__auth'>
+            <li className='header__auth-item link'>
+              <button type='button' className='header__button link'>
+                <Link to='/signup' className='header__button-link' onClick={openSignUpPopup}>
+                  Регистрация
+                </Link>
+              </button>
+            </li>
+            <li className='header__auth-item link'>
+              <button type='button' className='header__button link'>
+                <Link to='/signin' className='header__button-link' onClick={openSignInPopup}>
+                  Вход
+                </Link>
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
