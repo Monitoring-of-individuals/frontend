069e7bb5a58de248e6144b26a83ad4c7affb3fb6
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-03-16 01:53:39 +0300

    sources and about blocks

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import './About.css';
+import manager from '../../images/manager.svg';
+import car from '../../images/car.svg';
+import laptop from '../../images/laptop.svg';
 
 const About: FC = (): React.ReactElement => {
   return (
@@ -9,20 +12,35 @@ const About: FC = (): React.ReactElement => {
         <div className='about__columns'>
           <div className='about__column'>
             <div className='about__column-container'>
+              <div className='about__image-background'>
+                <div className='about__image-container'>
+                  <img src={manager} alt="logo" className='about__image-img' />
+                </div>
+              </div>
               <h3 className='about__column-title'>Сотрудника</h3>
-              <p className='about__column-text'>Не находится в розыске,не имеет долгов</p>
+              <p className='about__column-text'>Подтверждение документов, наличие действующих исполнительных делопроизводств</p>
             </div>
           </div>
           <div className='about__column'>
             <div className='about__column-container'>
+              <div className='about__image-background'>
+                <div className='about__image-container'>
+                  <img src={car} alt="logo" className='about__image-img' />
+                </div>
+              </div>
               <h3 className='about__column-title'>Водителя</h3>
-              <p className='about__column-text'>Не лишён ли водительских прав, не имеет серьезных нарушений ПДД</p>
+              <p className='about__column-text'>Действительность водительских прав, стаж вождения, страховые случаи, аварии</p>
             </div>
           </div>
           <div className='about__column'>
             <div className='about__column-container'>
+              <div className='about__image-background'>
+                <div className='about__image-container'>
+                  <img src={laptop} alt="logo" className='about__image-img' />
+                </div>
+              </div>
               <h3 className='about__column-title'>Самозанятого</h3>
-              <p className='about__column-text'>Есть статус самозанятого, не находится в розыске, нет задолженностей</p>
+              <p className='about__column-text'>Наличие официального статуса, исполнительных делопроизводств, судимостей</p>
             </div>
           </div>
         </div>
