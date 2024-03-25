d2379b9db781130b0c3f95ee0af8ef245440c285
Dmitrii Romanov <RomDim197@gmail.com>
2024-03-26 01:12:24 +0300

    WIP popup styles

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,6 +9,7 @@ const USER_RESET_FAILURE: 'USER_RESET_FAILURE' = 'USER_RESET_FAILURE';
 const EMAIL_REGEXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$/;
 const PATTERN_NAME = /^(?=.{1,30}$)[а-яёА-ЯЁ]+(?:[-][а-яёА-ЯЁ]+)*$/;
 const PATTERN_PASSPORT = /^(\d *){10}$/;
+const PATTERN_PASSWORD = /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*/;
 
 export {
   GET_USER_CREDENTIALS,
@@ -19,4 +20,5 @@ export {
   EMAIL_REGEXP,
   PATTERN_NAME,
   PATTERN_PASSPORT,
+  PATTERN_PASSWORD,
 };
